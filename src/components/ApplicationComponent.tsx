9a04bcb4fb5122682d0e957097934577950e9160
Frederik Batuna <fbatuna@key-biz.it>
2021-02-27 22:47:19 +0100

    add: edit application after

--- src/components/ApplicationComponent.tsx
+++ src/components/ApplicationComponent.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import { TermPath } from "../core/path";
+import { getByPath, isSamePath, setByPath, TermPath } from "../core/path";
 import { Application } from "../core/program";
 import { Hoverable } from "./Hoverable";
 import { Details } from "./Details";
-import { ProgramContext, colors } from "../App";
+import { ProgramContext, colors, EditorContext } from "../App";
 import { TermComponent } from "./TermComponent";
 
 export function ApplicationComponent({
@@ -15,16 +15,57 @@ export function ApplicationComponent({
   parens: boolean;
   path: TermPath;
 }) {
-  const { hasError } = React.useContext(ProgramContext);
+  const editor = React.useContext(EditorContext);
+  const { hasError, getType } = React.useContext(ProgramContext);
   const [hasMouseOver, setHasMouseOver] = React.useState(false);
+  const hasCursor = isSamePath(path, editor.state.cursor);
+  const type = getType(term);
+  const hasType = type !== null;
+  React.useLayoutEffect(() => {
+    if (!hasCursor) return;
+    const onKeyDown = (event: KeyboardEvent) => {
+      const parentPath = path.slice(0, -1);
+      const leafPath = path[path.length - 1];
+      const parent = getByPath(parentPath, editor.program);
+      if (event.key === " ") {
+        debugger;
+        if (parent?.type === "application" && leafPath === "right") {
+          event.preventDefault();
+          const program = setByPath(
+            parentPath,
+            {
+              type: "application",
+              left: parent,
+              right: { type: "reference", reference: "" },
+            },
+            editor.program
+          );
+          if (program) {
+            editor.action.setProgram(program);
+            editor.action.setCursor([...parentPath, "right"]);
+          }
+        }
+      }
+    };
+    document.addEventListener("keydown", onKeyDown);
+    return () => {
+      document.removeEventListener("keydown", onKeyDown);
+    };
+  }, [hasCursor]);
   return (
     <Hoverable
       hasMouseOver={hasMouseOver}
       head={
         <span
           style={{
-            backgroundColor: hasMouseOver ? colors.backgroundDark : "",
-            borderBottom: hasError(term) ? `2px solid ${colors.red}` : "",
+            backgroundColor:
+              hasMouseOver || hasCursor ? colors.backgroundDark : "",
+            borderBottom: hasError(term)
+              ? `2px solid ${colors.red}`
+              : !hasType
+              ? `2px solid ${colors.gray}`
+              : "2px solid transparent",
+            marginBottom: "-2px",
           }}
         >
           {parens && "("}
@@ -36,6 +77,7 @@ export function ApplicationComponent({
           <span
             onMouseOver={() => setHasMouseOver(true)}
             onMouseLeave={() => setHasMouseOver(false)}
+            onClick={() => editor.action.setCursor(path)}
           >
             {" "}
           </span>
